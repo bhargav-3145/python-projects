8e3a73c494c6d1d8d6cfb6a2c2dd64ae17856a34
Bhargav <sandabhargav3101@gmail.com>
2026-06-08 22:02:36 +0530

    Add attempt counter and replay feature

--- 01-number-guessing-game/number-guessing-game.py
+++ 01-number-guessing-game/number-guessing-game.py
@@ -1,11 +1,25 @@
 import random
-number = random.randint(1, 100)
-guess = int(input("Enter Your Guess : "))
-while guess != number :
-    if (guess > number):
-        print("Too high! Try a lower number")
-    elif(guess < number):
-        print("Too low! Try a higher number")
-    guess = int(input("Enter your next guess : "))
-else:
-    print("Congratulations, You guessed the correct number")
+player_wants_to_play = True
+while player_wants_to_play :
+    number = random.randint(1, 100)
+    count = 1
+    guess = int(input("Enter Your Guess : "))
+    while guess != number :
+        if (guess > number):
+            print("Too high! Try a lower number")
+        elif(guess < number):
+            print("Too low! Try a higher number")
+        guess = int(input("Enter your next guess : "))
+        count += 1
+    else:
+        print(f"Congratulations! You guessed the correct number in {count} attempts")
+    while True :
+        replay = input("Play again? (Yes or No) : ").lower()
+        if replay == "yes":
+            break
+        elif replay == "no":
+            player_wants_to_play = False
+            break
+        else :
+            print("Please enter only Yes or No.")
+
